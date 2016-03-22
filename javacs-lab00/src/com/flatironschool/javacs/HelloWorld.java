783a01a2d4d2e77ab835bdfd454de0e4d40533af
Simon Posada Fishman <simon.posada29@gmail.com>
2016-03-21 20:37:18 -0700

    Done.

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -2,10 +2,11 @@ package com.flatironschool.javacs;
 
 public class HelloWorld {
     public static Double getVersion() {
-        // I won't work!
+        String version = System.getProperty("java.specification.version");
+        return Double.parseDouble(version);
     }
 
     public static void main(String[] args) {
-	    // you can test the output of getVersion() here
+	    System.out.println(getVersion());
     }
 }
